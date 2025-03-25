2bdf0cd09c629763ee4b7ca1ba7368dc47dc6618
krypto5863 <29824718+krypto5863@users.noreply.github.com>
2025-03-25 14:37:42 -0400

    Removed dep

--- Experiment5ParallelMenuLoading/Loaders/VanillaMenuLoad.cs
+++ Experiment5ParallelMenuLoading/Loaders/VanillaMenuLoad.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using COM3D2API.Utilities;
-using Unity.Burst;
 using UnityEngine;
 
 namespace ShortMenuLoader.Loaders
